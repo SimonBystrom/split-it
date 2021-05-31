bd148b2eae1b70604156a3fcb3946b4114d04773
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-31 18:47:58 +0900

    Fixed migration table order so we can now migrate

--- db/migrate/20210531085611_change_default_value_in_payment.rb
+++ db/migrate/20210531085611_change_default_value_in_payment.rb
@@ -1,4 +1,5 @@
 class ChangeDefaultValueInPayment < ActiveRecord::Migration[6.1]
-  def change_column_null :payment_groups, :name, false
+  def change
+    change_column_null :payment_groups, :name, false
   end
 end
