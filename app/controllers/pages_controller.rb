8d2adcbed87e32105f83d7091df418bb71bd33cc
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-01 12:08:43 +0900

    Added associations to User model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @payment_groups = PaymentGroups.all
+    @splits = current_user.splits
+  end
+
+  def splash
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :memberships
+  has_many :payment_groups, through: :memberships
+  has_many :splits, through: :payment_groups
+  has_many :bills
 end
