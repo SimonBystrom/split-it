d9a8c5bec8f7f5f5e096e3fb7d4d9423bd161b5c
abdul muhaimin <muhai07@outlook.com>
2021-06-02 16:39:54 +0900

    updated form and code fixes

--- app/controllers/payment_groups_controller.rb
+++ app/controllers/payment_groups_controller.rb
@@ -21,7 +21,7 @@ class PaymentGroupsController < ApplicationController
     authorize @payment_group
     if @payment_group.save
       flash[:success] = "Payment Group successfully created"
-      redirect_to @show
+      redirect_to payment_group_path(@payment_group)
     else
       flash[:error] = "Something went wrong"
       render 'new'
@@ -43,6 +43,6 @@ class PaymentGroupsController < ApplicationController
   end
 
   def payment_group_params
-    params.require(:payment_group).permit(:name, :description)
+    params.require(:payment_group).permit(:name, :description, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :payment_groups, only: [:index, :show, :new, :edit, :update] do
+  resources :payment_groups, only: [:index, :show, :new, :edit, :update, :create] do
     resources :splits, only: [:new]
   end
   resources :splits, only: [:show, :edit] do
