0d8caf1a6b856b47672f18a2242d3b2fee67ae4e
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-06-03 15:41:38 +0900

    fixed the form and now it works

--- app/controllers/splits_controller.rb
+++ app/controllers/splits_controller.rb
@@ -13,12 +13,13 @@ class SplitsController < ApplicationController
   def create
     @split = Split.new(splits_params)
     authorize @split
+    @payment_group = PaymentGroup.find(params[:payment_group_id])
+    @split.payment_group = @payment_group
     if @split.save
       flash[:success] = "Split successfully created"
       redirect_to @split
     else
       flash[:error] = "Something went wrong"
-      render 'payment_groups/show'
     end
   end
 
@@ -36,7 +37,7 @@ class SplitsController < ApplicationController
   private
 
   def splits_params
-    params.require(:split).permit(:total, :active, :split_ratio)
+    params.require(:split).permit(:name)
   end
 
   def set_split
