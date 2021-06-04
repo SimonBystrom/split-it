9edffab328e702110429cd84e7aaf5fcbb07f68b
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-04 12:26:29 +0900

    fixed logic for multiple users on user balance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,13 @@ module ApplicationHelper
   def get_split_total(split)
     split.bills.sum(&:price)
   end
-
+  
   def get_user_split_balance(split)
     @balance = get_balance(split)
     @balance.positive? ? "You claim $#{@balance}" : "You pay $#{@balance}"
   end
 
   def balance_positive?(split)
-    @balance = get_balance(split)
     get_balance(split).positive?
   end
 
@@ -17,7 +16,7 @@ module ApplicationHelper
 
   def get_balance(split)
     user_split_total = split.bills.where(user: current_user).sum(&:price)
-    even_split = split.bills.sum(&:price)/2
+    even_split = split.bills.sum(&:price)/split.users.count
     user_split_total - even_split
   end
 end
