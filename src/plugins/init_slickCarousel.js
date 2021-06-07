72746bfd544c3430976be3182c764995eacc685e
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-06-07 14:38:27 +0900

    slick is deprecated - switched to glide

--- src/plugins/init_slickCarousel.js
+++ src/plugins/init_slickCarousel.js
@@ -4,6 +4,10 @@ const initSlickCarousel = () => {
   $(document).ready(function(){
     $('.carousel-scroller').slick({
     //attribute: value;
+    // slide: document.querySelectorAll(".carousel-group");
+    variableWidth: true,
+    mobileFirst: true,
+    slidesToShow: 8,
     });
   });
 };
