df525fd074f8c7af0682ca8e1b5a1240c049c3c0
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-10 14:13:51 +0900

    Quick validation fix

--- app/models/payment_group.rb
+++ app/models/payment_group.rb
@@ -4,4 +4,6 @@ class PaymentGroup < ApplicationRecord
   has_many :users, through: :memberships
   has_one_attached :photo
   has_secure_token
+
+  validates :description, length: {minimum:0, maximum:50}
 end
